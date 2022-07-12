2eff298dc160b242e078f1ce0ac078c10b4fb102
Alex Lebedko <lebedko@soramitsu.co.jp>
2022-07-12 17:27:53 +0700

    IconGenerating eth case removed

--- FearlessUtils/Classes/Icon/IconGenerating.swift
+++ FearlessUtils/Classes/Icon/IconGenerating.swift
@@ -1,20 +1,9 @@
 import Foundation
 
-enum IconGeneratingError: Error {
-    case notImplemented
-}
-
 public protocol DrawableIcon {
     func drawInContext(_ context: CGContext, fillColor: UIColor, size: CGSize)
 }
 
 public protocol IconGenerating {
     func generateFromAddress(_ address: String) throws -> DrawableIcon
-    func ethereumIconFromAddress(_ address: String) throws -> DrawableIcon
-}
-
-public extension IconGenerating {
-    func ethereumIconFromAddress(_ address: String) throws -> DrawableIcon {
-        throw IconGeneratingError.notImplemented
-    }
 }
