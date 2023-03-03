d0f7a11fb71bf7bfc567fed920daae1c33b29ae6
Radmir Dzhurabaev <dzhurabaev@soramitsu.co.jp>
2023-03-03 11:36:54 +0800

    BytesCodable added in CodableWrappers

--- FearlessUtils/Classes/Scale/CodableWrappers.swift
+++ FearlessUtils/Classes/Scale/CodableWrappers.swift
@@ -99,3 +99,26 @@ public struct NullCodable<T: Codable>: Codable {
 }
 
 extension NullCodable: Equatable where T: Equatable {}
+
+@propertyWrapper
+public struct BytesCodable: Codable, Equatable {
+    public var wrappedValue: Data
+
+    public init(wrappedValue: Data) {
+        self.wrappedValue = wrappedValue
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let byteArray = try container.decode([StringScaleMapper<UInt8>].self)
+
+        wrappedValue = Data(byteArray.map(\.value))
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        let bytes = wrappedValue.map { StringScaleMapper(value: $0) }
+
+        try container.encode(bytes)
+    }
+}
